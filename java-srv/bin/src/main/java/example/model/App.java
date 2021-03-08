a53e0e74efcb47c726401c04b23956db37f67e47
bearddan2000 <67988550+bearddan2000@users.noreply.github.com>
2021-03-07 22:53:34 -0500

    Add files via upload

---
+++ java-srv/bin/src/main/java/example/model/App.java
@@ -0,0 +1,10 @@
+package example.model;
+
+public class App {
+    public String getTitle()  {
+        return "Hello Websocket";
+    }
+    public String getBanner()  {
+        return "Websocket";
+    }
+}
